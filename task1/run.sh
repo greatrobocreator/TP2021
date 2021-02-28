bc9fb701e6875cd4b65520e60695b49ac9646367
GreatRoboCreator <greatrobocreator@gmail.com>
2021-02-28 13:46:58 -0500

    First test

--- task1/run.sh
+++ task1/run.sh
@@ -5,9 +5,9 @@ counter=0
 
 for i in $(find $1 -name "*.$2" -type f -printf "%f\n")
 do
-    echo $counter $i
     cp "$PWD/$i" "$PWD/$3/${counter}_$i"
     ((counter++))
 done
 
 tar -czf $4 $3/
+echo "done"
